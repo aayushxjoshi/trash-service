0471dfdeea07a1b71becca34425a0b83f51e9a05
aayushxjoshi <aayushj9173@gmail.com>
2026-05-09 12:00:26 +0530

    add multiuser

--- sockets/index.ts
+++ sockets/index.ts
@@ -11,23 +11,75 @@ const io = new Server(server, {
   },
 });
 
+const users = new Map<string, string>();
+
+const messages: {
+  user: string;
+  text: string;
+  time: string;
+}[] = [];
+
 app.get("/", (_, res) => {
   res.send("Chat server running");
 });
 
+app.get("/messages", (_, res) => {
+  res.json(messages);
+});
+
+app.get("/users", (_, res) => {
+  res.json([...users.values()]);
+});
+
 io.on("connection", (socket) => {
   console.log("User connected:", socket.id);
-  socket.on("message", (message) => {
-    console.log("Message:", message);
-    io.emit("message", {
-      id: socket.id,
-      text: message,
-      time: new Date(),
-    });
+
+  socket.on("join", (username: string) => {
+    users.set(socket.id, username);
+
+    console.log(`${username} joined`);
+
+    io.emit("system", `${username} joined the chat`);
+
+    io.emit("users", [...users.values()]);
+  });
+
+  socket.on("message", (text: string) => {
+    const username = users.get(socket.id) || "Anonymous";
+
+    const message = {
+      user: username,
+      text,
+      time: new Date().toLocaleTimeString(),
+    };
+
+    messages.push(message);
+
+    if (messages.length > 50) {
+      messages.shift();
+    }
+
+    console.log(message);
+
+    io.emit("message", message);
+  });
+
+  socket.on("typing", () => {
+    const username = users.get(socket.id);
+
+    socket.broadcast.emit("typing", `${username} is typing...`);
   });
 
   socket.on("disconnect", () => {
-    console.log("User disconnected:", socket.id);
+    const username = users.get(socket.id);
+
+    console.log("Disconnected:", username);
+
+    users.delete(socket.id);
+
+    io.emit("system", `${username} left the chat`);
+
+    io.emit("users", [...users.values()]);
   });
 });
 
